9356a99c793255fbc47d55887c1b7abd6aae48b5
ParkJongJoon <whdwnsdk8111@naver.com>
2021-01-29 16:24:15 +0900

    Android_studio_practice_by_Lemon

---
+++ app/src/main/java/com/example/ex3/ex3/MainActivity_ex3.kt
@@ -0,0 +1,39 @@
+package com.example.ex3.ex3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.ex3.R
+import kotlinx.android.synthetic.main.activity_main_ex3.*
+
+class MainActivity_ex3 : AppCompatActivity() {
+
+    private var fee = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_ex3)
+
+        pay_self.setOnClickListener {
+            var input_money = input_pay.text.toString().toInt()
+            var result = text4.text.toString().toInt() + input_money
+            text4.setText(result.toString())
+
+            fee += 1200
+            text6.setText(fee.toString())
+        }
+
+        pay_someone.setOnClickListener {
+            var input_money = input_pay.text.toString().toInt()
+            var current_money = text4.text.toString().toInt()
+            if (input_money > current_money) {
+                Toast.makeText(applicationContext, "출금 불가", Toast.LENGTH_SHORT).show()
+            } else {
+                var result = current_money - input_money
+                text4.setText(result.toString())
+                fee += 1200
+                text6.setText(fee.toString())
+            }
+        }
+    }
+}
