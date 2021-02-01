fc632ea3ba79a1021484f6f00e2ab64d47172592
ParkJongJoon <whdwnsdk8111@naver.com>
2021-02-01 17:36:53 +0900

    Android_studio_practice_by_Lemon

--- app/src/main/java/com/example/ex3/ex3/MainActivity_ex3.kt
+++ app/src/main/java/com/example/ex3/ex3/MainActivity_ex3.kt
@@ -18,26 +18,34 @@ class MainActivity_ex3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_ex3)
 
+        // 입금 버튼 클릭시
         pay_self.setOnClickListener {
+            // null 체크
             if (input_pay.text.count() > 1) {
                 var editMoney: Int = input_pay.text.toString().toInt()
                 plusMoney(editMoney)
             } else {
+                //null일 경우 기본값 0 반환
                 plusMoney()
             }
         }
+
+        // 출금 버튼 클릭시
         pay_someone.setOnClickListener {
+            // null 체크
             if (input_pay.text.count() > 1) {
                 var editMoney: Int = input_pay.text.toString().toInt()
                 minusMoney(editMoney)
             } else {
+                //null일 경우 기본값 0 반환
                 minusMoney()
             }
         }
     }
 
-
+    // 입금 메소드
     private fun plusMoney(editMoney: Int = 0) {
+        // 입력금액이 수수료보다 많을때
         if (editMoney > bankFees) {
             totalMoney += (editMoney - bankFees)
             text4.text = totalMoney.toString()
@@ -49,22 +57,27 @@ class MainActivity_ex3 : AppCompatActivity() {
         }
     }
 
-
+    // 출금 메소드
     private fun minusMoney(editMoney: Int = 0) {
         val estimatedAmount: Int = (editMoney + bankFees)
 
         when {
+            // 입력금액보다 수수료가 많을 때
             editMoney <= bankFees -> {
                 Toast.makeText(applicationContext, "출금액 부족입니다", Toast.LENGTH_SHORT).show()
             }
 
+            // 총금액이 수수료보다 적을 때
             totalMoney <= bankFees -> {
                 Toast.makeText(applicationContext, "수수료 부족입니다", Toast.LENGTH_SHORT).show()
             }
 
+            // (입력금액 + 수수료)가 총금액보다 많을 때
             estimatedAmount >= totalMoney -> {
                 Toast.makeText(applicationContext, "한도 초과입니다", Toast.LENGTH_SHORT).show()
             }
+
+            // (입력금액 + 수수료)가 총금액보다 적을 때
             estimatedAmount < totalMoney -> {
                 totalMoney -= estimatedAmount
                 Toast.makeText(
