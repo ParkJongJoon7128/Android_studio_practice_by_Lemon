ebd60ce414c5662ae2570e1bb8703ddb7d052f2d
ParkJongJoon <whdwnsdk8111@naver.com>
2021-02-02 17:44:48 +0900

    Android_studio_practice_by_Lemon

---
+++ app/src/main/java/com/example/ex3/ex5/FourthActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ex3.ex5
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ex3.R
+
+class FourthActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.noneMemo)
+
+
+
+    }
+}
--- app/src/main/java/com/example/ex3/ex5/MainActivity_ex5.kt
+++ app/src/main/java/com/example/ex3/ex5/MainActivity_ex5.kt
@@ -1,12 +1,24 @@
 package com.example.ex3.ex5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ex3.R
+import kotlinx.android.synthetic.main.activity_main_ex5.*
 
 class MainActivity_ex5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_ex5)
+
+        btn_writeMemo.setOnClickListener {
+            var intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_seeMemo.setOnClickListener {
+
+        }
+
     }
 }
