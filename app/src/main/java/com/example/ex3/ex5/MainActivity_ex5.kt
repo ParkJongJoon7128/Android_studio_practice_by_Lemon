449d189783aad0fb301bb99b8bd059650c0ec459
ParkJongJoon <whdwnsdk8111@naver.com>
2021-02-03 17:12:51 +0900

    Android_studio_practice_by_Lemon

--- app/src/main/java/com/example/ex3/ex5/MainActivity_ex5.kt
+++ app/src/main/java/com/example/ex3/ex5/MainActivity_ex5.kt
@@ -1,33 +1,11 @@
 package com.example.ex3.ex5
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.example.ex3.R
-import kotlinx.android.synthetic.main.activity_main_ex5.*
-import kotlinx.android.synthetic.main.list_item.*
 
 class MainActivity_ex5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_ex5)
-
-        btn_writeMemo.setOnClickListener {
-            var intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
-        }
-
-        btn_seeMemo.setOnClickListener {
-            var intent = Intent(this, ThridActivity::class.java)
-            startActivity(intent)
-        }
-
-        var title = intent.getStringExtra("title")
-        var content = intent.getStringExtra("content")
-
-        textView1.text = title
-        textView2.text = content
-
     }
 }
